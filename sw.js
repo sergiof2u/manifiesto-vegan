24c0a0232a9ddd7424e2c382ba68545ec34696b8
sergiof2u <adharajoyas@gmail.com>
2025-06-09 18:21:01 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('vegano-app').then(cache => {
       return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './vegan-icon.png'
+        '/manifiesto-vegan/',
+        '/manifiesto-vegan/index.html',
+        '/manifiesto-vegan/manifest.json',
+        '/manifiesto-vegan/vegan-icon.png'
       ]);
     })
   );
